1483e96af32fdf6e6b507a63dbde4c58983f4a53
YoussefSoliman96 <youssef@DESKTOP-757K3CN>
2023-09-28 22:12:05 +0300

    modify admin page

--- auctions/models.py
+++ auctions/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Category(models.Model):
+    title = models.CharField(max_length=60)
+
 class AuctionListing(models.Model):
     title = models.CharField(max_length=60)
     price = models.FloatField()
@@ -12,4 +15,6 @@ class AuctionListing(models.Model):
     imageUrl = models.CharField(max_length=600)
     active = models.BooleanField(default=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category", null=True)
+
+
